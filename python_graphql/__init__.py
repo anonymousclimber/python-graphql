7e3e5fec350ab2ae1fa26a166e6b37f8d159b0a3
Anand <anandsamra55@gmail.com>
2025-09-01 13:32:17 +0200

    DB now working

--- python_graphql/__init__.py
+++ python_graphql/__init__.py
@@ -1,8 +1,17 @@
 from flask import Flask
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from .constants import name, pw
 
 app = Flask(__name__)
 CORS(app)
+
+# Databse Connections. 
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://"+name+":"+pw+"@localhost:5432/graphql"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+
 
 @app.route('/')
 def hello():
